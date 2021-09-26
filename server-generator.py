d1096a99193d63bf113c5eb50a9ca5caf4fb6a33
Lauri Alanen <80245457+LauriAlanen@users.noreply.github.com>
2021-09-26 17:50:14 +0300

    Update server-generator.py

--- server-generator.py
+++ server-generator.py
@@ -2,73 +2,100 @@ import os
 import webbrowser
 import requests
 import time
-import ctypes
-import sys
+
+class Actions:
 
 
-def add_rules():
-    os.system(f"""netsh advfirewall firewall add rule name="Valheim-server TCP 2456-2458" dir=in action=allow protocol=TCP localport=2456-2458""")
-    os.system(f"""netsh advfirewall firewall add rule name="Valheim-server UDP 2456-2458" dir=in action=allow protocol=UDP localport=2456-2458""")
+    def __init__(self, server_name, world_name, password):
+        self.server_name = server_name
+        self.world_name = world_name
+        self.password = password
+        self.filename = "start_headless_server.bat"
+        
+
+    def add_rules(self):
+        os.system(f"""netsh advfirewall firewall add rule name="Valheim-server TCP 2456-2458" dir=in action=allow protocol=TCP localport=2456-2458""")
+        os.system(f"""netsh advfirewall firewall add rule name="Valheim-server UDP 2456-2458" dir=in action=allow protocol=UDP localport=2456-2458""")
 
 
-def delete_rules():
-    os.system(f"""netsh advfirewall firewall delete rule name="Valheim-server TCP 2456-2458" protocol=TCP localport=2456-2458""")
-    os.system(f"""netsh advfirewall firewall delete rule name="Valheim-server UDP 2456-2458" protocol=UDP localport=2456-2458""")
+    def delete_rules(self):
+        os.system(f"""netsh advfirewall firewall delete rule name="Valheim-server TCP 2456-2458" protocol=TCP localport=2456-2458""")
+        os.system(f"""netsh advfirewall firewall delete rule name="Valheim-server UDP 2456-2458" protocol=UDP localport=2456-2458""")
 
 
-def wlan_site():
-    try:
-        requests.get("http://192.168.0.1/")
-        webbrowser.open("http://192.168.0.1/")
+    def wlan_site(self):
+        try:
+            requests.get("http://192.168.0.1/")
+            webbrowser.open("http://192.168.0.1/")
 
 
-    except requests.exceptions.ConnectionError:
-        requests.get("http://192.168.1.1/")
-        webbrowser.open("http://192.168.1.1/") 
+        except requests.exceptions.ConnectionError:
+            requests.get("http://192.168.1.1/")
+            webbrowser.open("http://192.168.1.1/") 
 
 
-def file_write():
+    def locate_file(self):
 
-    f_name = "start_headless_server.bat"
-    first = time.perf_counter()
-    state = False
+        first = time.perf_counter()
 
-    for root, dirs, files in os.walk("C:\\"):
-        if f_name in files:
-            state = True
-            file_path = os.path.join(root, f_name)
-            print(file_path)
-            break
+        for root, dirs, files in os.walk("C:\\"):
+            if self.filename in files:
+                file_path = os.path.join(root,self.filename)
+                return print(file_path)
 
 
-        else:
-            second = time.perf_counter()
-            difference = second - first
+            else:
+                second = time.perf_counter()
+                difference = second - first
 
-            if(difference > 10):
-                print("Failed to locate the file please enter it manually!")
-                state = False
-                break
+                if(difference > 10):
+                    print("Failed to locate the file!")
+                    
 
 
-def is_admin():
+
+
+
+#def file_write(server_name, world, password):
+#    file = open(locate_file(), "r")
+#    read_file = file.read()
+#    splitted = read_file.split("\n")
+#    splitted.pop(-1)
+#    splitted.append(f"""valheim_server -nographics -batchmode -name "{server_name}" -port 2456 -world "{world}" -password "{password}" """)
+#    file.close()
+
+#    file = open(locate_file(), "w")
+    
+#    for i in range (len(splitted)):
+#        file.write(splitted[i] + "\n")
+#    file.close()
+#    print("Kirjoitettu!")
+#    quit()
+
+
+
+
+
+new = Actions("lmao", "ok",123)
+new.locate_file()
+
+
+
+
+
+
+
+
+
+"""def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
-
     except:
-        return False
+        return False"""
 
 
-if is_admin():
-    x = int(input("1 add 2 delete 3 find file 4 web"))
-    if (x == 1):
-        add_rules()
-    if (x == 2):
-        delete_rules()
-    if (x == 3):
-        file_write()
-    if(x == 4):
-        wlan_site()
+"""if is_admin():"""
 
-else:
-    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None,1)
+
+"""else:
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None,1)"""
